9ff41295b5055606cb7297210ec80d2db14833e4
tomaruz <tomaruz2002@gmail.com>
2023-04-24 20:29:46 +0100

    cursorneedsfixing

--- experiments.js
+++ experiments.js
@@ -1,3 +1,25 @@
+//CURSOR
+
+jQuery(document).ready(function() {
+
+    var mouseX = 0, mouseY = 0;
+    var xp = 0, yp = 0;
+     
+    $(document).mousemove(function(e){
+      mouseX = e.pageX - 25; //distance from mouse current x position
+      mouseY = e.pageY - 25; //distance from mouse current y position
+    });
+      
+    setInterval(function(){
+      xp += ((mouseX - xp)/4); //speed how quickly the circle comes back to the x current 
+      yp += ((mouseY - yp)/4);
+      $("#circle").css({left: xp +'px', top: yp +'px'});
+    }, 20);
+  
+  });
+
+
+
 
 //PARALLAX EFFECT FOR THE FACE
 
